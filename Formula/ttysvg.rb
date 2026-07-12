79bf71e887462348461613a5944c49087ab85a7e
Polina <36280649+rabarbra@users.noreply.github.com>
2026-07-12 17:09:24 +0200

    ttysvg: bump v0.1.0

--- Formula/ttysvg.rb
+++ Formula/ttysvg.rb
@@ -1,30 +1,30 @@
 class Ttysvg < Formula
   desc "Record terminal sessions as SVG animations"
   homepage "https://github.com/rabarbra/ttysvg"
-  version "0.0.9"
+  version "0.1.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/rabarbra/ttysvg/releases/download/v#{version}/ttysvg_v#{version}_macos_arm64.tar.gz"
-      sha256 "a289131848743cbf0eaf207384896539db939d71e73a4376d109ff9c04235bca"
+      sha256 "d2e2a6b2626b56d7ba179957fa97c002160a9e4afca84846a3fb7fbcad363913"
     end
 
     on_intel do
       url "https://github.com/rabarbra/ttysvg/releases/download/v#{version}/ttysvg_v#{version}_macos_x86_64.tar.gz"
-      sha256 "f853f7ff8aa788cfce5d32b5a71389c975792128ff496cfb8064549ba7d85701"
+      sha256 "3cba1bffda1182e6722ec02e059731a5e46fab05f7da0e1f25868a72e6f2f51b"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/rabarbra/ttysvg/releases/download/v#{version}/ttysvg_v#{version}_linux_arm64.tar.gz"
-      sha256 "102b95a2d1218d3469516a38024e16e400dfc7c713f1141385eae9599132c41a"
+      sha256 "8581c22239614dc1ae0a68ac34fcbd06b51c5617667bd5b14503a5c3225c06c0"
     end
 
     on_intel do
       url "https://github.com/rabarbra/ttysvg/releases/download/v#{version}/ttysvg_v#{version}_linux_x86_64.tar.gz"
-      sha256 "aca6ce789c05ec13a7ce7c0683375a2851700593c7d35d65232ea518f806b0b5"
+      sha256 "9a8b83e7be3adaf9144725186dcd39f3ad34be614d693fecc394604f7b6342ab"
     end
   end
 
