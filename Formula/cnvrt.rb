9c5dfb4e902a7ddcc3627c12a3096a05473499c1
rabarbra <36280649+rabarbra@users.noreply.github.com>
2026-07-19 07:09:22 +0000

    cnvrt: bump v0.0.2

--- Formula/cnvrt.rb
+++ Formula/cnvrt.rb
@@ -1,7 +1,7 @@
 class Cnvrt < Formula
   desc "Interactive CLI for file conversion"
   homepage "https://github.com/shellcell/cnvrt"
-  version "0.0.1"
+  version "0.0.2"
   license "MIT"
 
   livecheck do
@@ -12,24 +12,24 @@ class Cnvrt < Formula
   on_macos do
     on_arm do
       url "https://github.com/shellcell/cnvrt/releases/download/v#{version}/cnvrt-v#{version}-darwin-arm64.tar.gz"
-      sha256 "4449c7048d18390e373fdad0afa758da81c8d0167ea4816d1d3baee48c72a3c9"
+      sha256 "371431d0ef8fd3d2f07d2d9e7e3369d734be2733bbe6da4ba1b00854d3ce1617"
     end
 
     on_intel do
       url "https://github.com/shellcell/cnvrt/releases/download/v#{version}/cnvrt-v#{version}-darwin-amd64.tar.gz"
-      sha256 "37bcf3ba2e4d6a03ffc0b7d208420fad496671a2872058f26b70afe2082f7e18"
+      sha256 "6e2fc4fc6cf762893ab7b85f3444d5899786c04fcf1f809524af8953d6fb2e3d"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/shellcell/cnvrt/releases/download/v#{version}/cnvrt-v#{version}-linux-arm64.tar.gz"
-      sha256 "5bd82fcafaa30637b6d822ba08b72fe56a08bd03f05af2c79d061cc7092b7fa5"
+      sha256 "eaaa8cd273585360c78b94162ddcefda30159e77dd48697f0fceb610b24f3b4e"
     end
 
     on_intel do
       url "https://github.com/shellcell/cnvrt/releases/download/v#{version}/cnvrt-v#{version}-linux-amd64.tar.gz"
-      sha256 "eebc64a3f1e12a6e99c0bf22db9587e7433c6e3121bdb3545f4148743dc32af8"
+      sha256 "6e0e18815afe900562f3e4bfb3c3fbd40ed1fcfdbfda80b735ff80bb53cc50f7"
     end
   end
 
